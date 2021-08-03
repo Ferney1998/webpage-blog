147b4aa704f835cd6161a6d765c7d8cd6b5263ee
wilmer <wcubides@unab.edu.co>
2021-08-02 21:38:11 -0500

    Styled navigation bar and posts

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
+  
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Wilmer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2021'
+    },
+    {
+        'author': 'Ferney',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2021'
+    }
+]
 
 
-# Create your views here.
 def home(request):
-	return HttpResponse('<h1>Blog Home</>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-	return HttpResponse('<h1>Blog Contact</>')
+    return render(request, 'blog/about.html', {'title': 'About'})
